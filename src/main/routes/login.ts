aa9ef430cfa9c7fe45a5c577964a32ad455140f6
gdiasdlima <gdiasdlima@yahoo.com.br>
2021-04-28 00:27:18 -0300

    feat: signIn route

--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { adaptRouter } from "../expressAdapter/expressAdapter";
 import { makeCreateLoginController } from "../factories/controller/createLoginControllerFactory";
+import { makeSignInController } from "../factories/controller/signInControllerFactory";
 
 
 export default (router: Router): void => {
 
     router.post('/createLogin', adaptRouter(makeCreateLoginController()))
+
+    router.post('/login/signIn',adaptRouter(makeSignInController()))
 }
