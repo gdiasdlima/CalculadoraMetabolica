63e66fec89715ee0862af91d74667c290e25911c
gdiasdlima <gdiasdlima@yahoo.com.br>
2021-04-28 00:13:20 -0300

    feat: signIn service

---
+++ src/data/services/signIn.ts
@@ -0,0 +1,28 @@
+import { Encrypter } from "../contracts/encrypter";
+import { ILoginRepository } from "../contracts/loginRepository";
+
+export class SignInService implements SignInUseCase {
+
+    constructor(
+        private loginRepository: ILoginRepository,
+        private cryptProvider: Encrypter
+    ) { }
+
+    async sign(data: SignInRequestModel): Promise<any> {
+
+        const alreadyLogin = await this.loginRepository.findByEmail(data.email);
+
+        if (!alreadyLogin) {
+            return new NotFoundError('participante');
+        }
+
+        if (!await this.cryptProvider.compare(data.password, alreadyLogin.password)) {
+            return new UnauthorizedError();
+        }
+
+        const loginPerfil = await this.loginPerfilRepository.findByLogin(alreadyLogin)
+
+        return { login: alreadyLogin, loginPerfil  }
+
+    }
+}
