a041032dcab3d4b4256c077b9599ebf06819fa1b
gdiasdlima <gdiasdlima@yahoo.com.br>
2021-04-29 13:10:59 -0300

    fix: refactor model login

--- src/data/models/login.ts
+++ src/data/models/login.ts
@@ -1,10 +1,15 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { PessoaModel } from './pessoa';
 
 @Entity('login')
 export class LoginModel {
   @PrimaryGeneratedColumn('increment')
   id: number
 
+  @OneToOne(() => PessoaModel)
+  @JoinColumn()
+  pessoa: PessoaModel;
+  
   @Column()
   email: string
 
