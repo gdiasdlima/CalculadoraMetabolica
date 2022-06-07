fe87d5fd0d2854e5d167d8e833e55698a8faa802
gdiasdlima <gdiasdlima@yahoo.com.br>
2022-06-06 21:45:47 -0300

    feat: interface tipoConquista repository

---
+++ src/data/contracts/repositories/tipoConquista.ts
@@ -0,0 +1,7 @@
+import { TipoConquista } from "../../entities/tipoConquista";
+
+export interface ITipoConquistaRepository {
+    create(data: TipoConquista): Promise<TipoConquista>
+    findByID(idPessoa: number) : Promise<TipoConquista>;
+    update(data: TipoConquista): Promise<void>;
+}
