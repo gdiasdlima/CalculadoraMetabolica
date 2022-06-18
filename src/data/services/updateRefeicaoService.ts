fd166778847a0c20b1afafa64d1d81c6064d8e6b
gdiasdlima <gdiasdlima@yahoo.com.br>
2022-06-18 19:52:15 -0300

    fix: updateRefeicaoService

--- src/data/services/updateRefeicaoService.ts
+++ src/data/services/updateRefeicaoService.ts
@@ -25,17 +25,21 @@ export class UpdateRefeicaoService implements UpdateRefeicaoUseCase {
             return new NotFoundError('pessoa');
         }
 
+        const AlreadyRefeicao = await this.refeicaoRepository.findByID(data.idRefeicao);
+        if (!AlreadyRefeicao) {
+            return new NotFoundError('Refeicao');
+        }
+        
         const refeicao = new Refeicao
         refeicao.tipoRefeicao = new TipoRefeicao()
         refeicao.pessoa = new Pessoa()
         refeicao.pessoa.id = data.idPessoa
         refeicao.id = data.idRefeicao
-        refeicao.carb = data.carb
-        refeicao.proteina = data.proteina
-        refeicao.gordura = data.gordura
-        refeicao.gramas = data.gramas
-        refeicao.data_refeicao = data.dataRefeicao
-        console.log(refeicao)
+        refeicao.carb = data.carb ? data.carb : AlreadyRefeicao.carb
+        refeicao.proteina = data.proteina ? data.proteina : AlreadyRefeicao.proteina
+        refeicao.gordura = data.gordura ? data.gordura : AlreadyRefeicao.gordura
+        refeicao.gramas = data.gramas ? data.gramas : AlreadyRefeicao.gramas
+        refeicao.data_refeicao = data.dataRefeicao ? data.dataRefeicao : AlreadyRefeicao.data_refeicao
         const response = await this.refeicaoRepository.update(refeicao)
 
         return response
