395b4cebeb69be2e05dd95eeee03eda1d55ecad4
gdiasdlima <gdiasdlima@yahoo.com.br>
2022-06-18 19:49:41 -0300

    feat: findbyid refeicao repository

--- src/data/contracts/repositories/refeicao.ts
+++ src/data/contracts/repositories/refeicao.ts
@@ -3,6 +3,7 @@ import { Refeicao } from "../../entities/refeicao";
 export interface IRefeicaoRepository {
     create(data: Refeicao): Promise<Refeicao>
     findByRefeicao(data: Refeicao) : Promise<Refeicao>;
+    findByID(data: number) : Promise<Refeicao>;
     getAllRefeicao(data: Refeicao) : Promise<Refeicao[]>;
     update(data: Refeicao): Promise<Refeicao>;
 }
--- src/infra/repositories/refeicaoRepository.ts
+++ src/infra/repositories/refeicaoRepository.ts
@@ -10,9 +10,17 @@ export class RefeicaoRepository implements IRefeicaoRepository {
         return await refeicaoRepository.save(data)
     }
 
+
     async findByRefeicao(data: Refeicao): Promise<Refeicao> {
         const refeicaoRepository = getRepository(Refeicao);
         const refeicao = await refeicaoRepository.findOne({ where: { pessoa: { id: data.pessoa.id }, tipoRefeicao: { id: data.tipoRefeicao.id }, data_refeicao: data.data_refeicao }, relations: ['tipoRefeicao', 'alimento'] });
+        return refeicao;
+    }
+
+    async findByID(id: number): Promise<Refeicao> {
+        const refeicaoRepository = getRepository(Refeicao);
+        const refeicao = await refeicaoRepository.findOne({ where: { id }, relations: ['TipoRefeicao']});
+
         return refeicao;
     }
 
