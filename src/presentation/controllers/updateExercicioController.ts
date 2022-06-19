68d1c02df4e92b4b5b53dec587a0b26e4c4b057a
gdiasdlima <gdiasdlima@yahoo.com.br>
2022-06-19 15:59:16 -0300

    feat: update exercicio Controller

---
+++ src/presentation/controllers/updateExercicioController.ts
@@ -0,0 +1,33 @@
+import { Controller } from '../contracts/controller'
+import { HttpRequest, HttpResponse } from '../contracts/http';
+import { badRequest, success, serverError } from '../contracts/httpHelper'
+import { Validator } from '../../validation/validator';
+import { UpdateExercicioUseCase } from '../../domain/useCases/updateExercicioUseCase';
+
+export class UpdatExercicioController implements Controller {
+    constructor(
+        private readonly validator: Validator,
+        private readonly updateRefeicaoUseCase: UpdateExercicioUseCase
+    ) {
+
+    }
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const error = this.validator.validate(httpRequest.body)
+            if (error) {
+                return badRequest(error)
+            }
+            const { idPessoa, dataExercicio, id, idTipoExercicio, kcalGastas, tempo } = httpRequest.body
+
+            const exercicio = await this.updateRefeicaoUseCase.update({ idPessoa, dataExercicio, id, idTipoExercicio, kcalGastas, tempo })
+
+            return success(exercicio)
+        }
+
+        catch (error) {
+            console.log(error)
+            return serverError()
+        }
+    }
+}
